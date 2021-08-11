d8dd0ecabdced1a917e008a148373eb05543a39a
Atush Bhalla <atushbhalla@email.arizona.edu>
2021-08-10 17:58:18 -0700

    UI

--- gsheet.py
+++ gsheet.py
@@ -1,6 +1,12 @@
 from riotwatcher import LolWatcher, ApiError
 from typing import List, Optional
 from pydantic import BaseModel
+import sys
+from PyQt5 import QtCore, QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QWidget, QLabel, QLineEdit
+from PyQt5.QtWidgets import QPushButton
+from PyQt5.QtCore import QSize  
+
 
 class Timeline(BaseModel):
     lane: str
@@ -66,6 +72,7 @@ class Match(BaseModel):
     teams: List[Team] = []
     participants: List[Participant] = []
 
+'''
 LOLWATCHER = LolWatcher('RGAPI-bc836c0e-e2cd-4468-b513-fd5800205fd3')
 REGION = 'na1'
 
@@ -82,3 +89,34 @@ def get_game_data_id(lol_watcher, region, game_id):
 curr = get_game_data_id(LOLWATCHER,REGION,4006452981)
 for i in curr:
     print(i)
+
+'''  
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        QMainWindow.__init__(self)
+
+        self.setMinimumSize(QSize(320, 140))    
+        self.setWindowTitle("LolGsheets") 
+
+        self.gameIdText = QLabel(self)
+        self.gameIdText.setText('GAME ID:')
+        self.gameId = QLineEdit(self)
+
+        self.gameId.move(80, 20)
+        self.gameId.resize(200, 32)
+        self.gameIdText.move(20, 20)
+
+        enterButton = QPushButton('ENTER', self)
+        enterButton.clicked.connect(self.clickMethod)
+        enterButton.resize(200,32)
+        enterButton.move(80, 60)        
+
+    def clickMethod(self):
+        print('Entered Game ID: ' + self.gameId.text())
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication(sys.argv)
+    mainWin = MainWindow()
+    mainWin.show()
+    sys.exit( app.exec_() )
