4d40ebc8bd2a584cd78d098547c033c05cdc1d58
Alec Miller <alecmmiller@email.arizona.edu>
2021-08-21 19:56:58 -0700

    Correct image spacing

--- gsheet.py
+++ gsheet.py
@@ -111,55 +111,61 @@ class MainWindow(QMainWindow):
         self.pixmap = QPixmap("Lane pngs/jungle.png")
         self.blueJungle.setPixmap(self.pixmap)
         self.blueJungle.resize(self.pixmap.width(), self.pixmap.height())
-        self.blueJungle.move(50,200)
+        self.blueJungle.move(50,240)
 
         self.blueMid = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/mid.png")
         self.blueMid.setPixmap(self.pixmap)
         self.blueMid.resize(self.pixmap.width(), self.pixmap.height())
-        self.blueMid.move(50,300)
+        self.blueMid.move(50,380)
 
         self.blueAdc = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/bottom.png")
         self.blueAdc.setPixmap(self.pixmap)
         self.blueAdc.resize(self.pixmap.width(), self.pixmap.height())
-        self.blueAdc.move(50,400)
+        self.blueAdc.move(50,520)
 
         self.blueSupport = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/support.png")
         self.blueSupport.setPixmap(self.pixmap)
         self.blueSupport.resize(self.pixmap.width(), self.pixmap.height())
-        self.blueSupport.move(50,500)
+        self.blueSupport.move(50,660)
 
         self.redTop = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/top.png")
         self.redTop.setPixmap(self.pixmap)
         self.redTop.resize(self.pixmap.width(), self.pixmap.height())
-        self.redTop.move(706,100)
+        self.redTop.move(830,100)
 
         self.redJungle = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/jungle.png")
         self.redJungle.setPixmap(self.pixmap)
         self.redJungle.resize(self.pixmap.width(), self.pixmap.height())
-        self.redJungle.move(706,200)
+        self.redJungle.move(830,240)
 
         self.redMid = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/mid.png")
         self.redMid.setPixmap(self.pixmap)
         self.redMid.resize(self.pixmap.width(), self.pixmap.height())
-        self.redMid.move(706,300)
+        self.redMid.move(830,380)
 
         self.redAdc = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/bottom.png")
         self.redAdc.setPixmap(self.pixmap)
         self.redAdc.resize(self.pixmap.width(), self.pixmap.height())
-        self.redAdc.move(706,400)
+        self.redAdc.move(830,520)
 
         self.redSupport = QLabel(self)
         self.pixmap = QPixmap("Lane pngs/support.png")
         self.redSupport.setPixmap(self.pixmap)
         self.redSupport.resize(self.pixmap.width(), self.pixmap.height())
-        self.redSupport.move(706,500)
+        self.redSupport.move(830,660)
+
+        self.redSupportArrow = QLabel(self)
+        self.pixmap = QPixmap("Lane pngs/up.png")
+        self.redSupportArrow.setPixmap(self.pixmap)
+        self.redSupportArrow.resize(self.pixmap.width(), self.pixmap.height())
+        self.redSupportArrow.move(600,680)
 
     def clickMethod(self):
         print('Entered Game ID: ' + self.gameId.text())
@@ -180,7 +186,7 @@ class MainWindow(QMainWindow):
                 if(i < 5):
                     label.move(200,yPos + space * i)
                 else:
-                    label.move(600, yPos - 500 + space * (i - 5))
+                    label.move(700, yPos - 500 + space * (i - 5))
                 label.show()
                 yPos+=100
                 i+=1
@@ -196,7 +202,7 @@ class MainWindow(QMainWindow):
         # label.show()
 
 
-LOLWATCHER = LolWatcher('RGAPI-01def34c-ba3e-4734-ae9a-8720145eee9f')
+LOLWATCHER = LolWatcher('RGAPI-4cdd9576-a891-4c2d-87f0-5c218ae9da17')
 REGION = 'na1'
 VERISION = LOLWATCHER.data_dragon.versions_for_region(REGION)
 CHAMPION_VERSIONS = VERISION['n']['champion']
